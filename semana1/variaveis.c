a6af7a05d8cc52ba8a55bfd729b86249c581d061
rodmg25-ctrl <rodmg25@gmail.com>
2026-07-29 16:46:29 -0300

    Adiciona hello.c e variaveis.c

---
+++ semana1/variaveis.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int idade = 30;
+    float altura = 1.72;
+    char inicial_nome = 'R';
+
+    printf("Rodrigo tem %d anos %.2f metros e seu nome começa com %c.\n", idade, altura, inicial_nome);
+}
